8c666d89215e8e24d24ad51102971b80aadb467e
Marijan Pezo <marijanpezo@yahoo.com>
2021-07-22 17:43:51 +0200

    Added busket button

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -32,7 +32,8 @@ class ProductController extends AbstractController{
         }
 
         return $this->render("details.html.twig", [
-            'bike' => $bike
+            'bike' => $bike,
+            'inBasket' => false
         ]);
     }
 }
