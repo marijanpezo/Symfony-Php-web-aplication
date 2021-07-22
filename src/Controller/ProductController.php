8d91a5826e610f8fa509efbc866ddc26f6d4f037
Marijan Pezo <marijanpezo@yahoo.com>
2021-07-22 19:28:40 +0200

    Basket form

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 use App\Repository\ProductRepository;
@@ -24,16 +26,26 @@ class ProductController extends AbstractController{
     /**
      * @Route("/products/{id}") 
      */
-    public function details($id, ProductRepository $repo): Response {
+    public function details($id, Request $request, ProductRepository $repo, SessionInterface $session): Response {
         $bike = $repo -> find($id);
 
         if ($bike === null){
             throw $this->createNotFoundException("The product does not exist!");
         }
 
+        // add to basket handler
+        $basket = $session->get('basket', []);
+
+        if ($request->isMethod('POST')) {
+            $basket[$bike->getId()] = $bike;
+            $session -> set('basket', $basket);
+        }
+
+        $isInBasket = array_key_exists($bike -> getId(), $basket);
+
         return $this->render("details.html.twig", [
             'bike' => $bike,
-            'inBasket' => false
+            'inBasket' => $isInBasket
         ]);
     }
 }
