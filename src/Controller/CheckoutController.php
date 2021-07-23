8fc2d3bde96f7cf18ff2ea16d43249fbed234f1a
Marijan Pezo <marijanpezo@yahoo.com>
2021-07-23 20:05:18 +0200

    Order entity, checkout Controller

---
+++ src/Controller/CheckoutController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\Routing\Annotation\Route;
+
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
+use App\Entity\Order;
+use App\Repository\ProductRepository;
+
+
+
+class CheckoutController extends AbstractController{
+
+    /**
+     * @Route("/checkout") 
+     */
+    public function checkout( Request $request, ProductRepository $repo, SessionInterface $session): Response {
+        
+        $basket = $session->get('basket', []);
+        $total = array_sum(array_map(function($product){ return $product->getPrice(); }, $basket));
+
+        $order = new Order;
+
+        $form = $this -> createFormBuilder($order)
+                ->add('name', TextType::class )
+                ->add('email', TextType::class)
+                ->add('address', TextType::class)
+                ->add('save', SubmitType::class, ['label' => 'Confirm order'])
+                ->getForm();
+
+        
+        return $this->render("checkout.html.twig", [
+            'total' => $total,
+            'form' => $form->createView()
+            
+        ]);
+    }
+}
---
+++ src/Entity/Order.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\Mapping as ORM;
+
+
+/**
+ * @ORM\Entity
+ * @ORM\Table{"orders"}
+ */
+class Order
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $name;
+
+        /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $address;
+
+    /**
+     * @ORM\ManyToMany(targetEntity="Product")
+     */
+    private $products;
+
+    public function __construct()
+    {
+        $this -> products = new ArrayCollection;
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getAddress(): ?string
+    {
+        return $this->address;
+    }
+
+    public function setAddress(?string $address): self
+    {
+        $this->address = $address;
+
+        return $this;
+    }
+
+    public function getProducts(): ?string
+    {
+        return $this->products;
+    }
+}
